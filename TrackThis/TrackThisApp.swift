be98c147c0f2780c64c1a99b7a2d60c4f7c2359f
Jacquese Whitson <jacqwhitsonappledev@gmail.com>
2023-07-17 22:21:22 -0400

    Initial Commit

---
+++ TrackThis/TrackThisApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackThisApp.swift
+//  TrackThis
+//
+//  Created by Jacquese Whitson on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct TrackThisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
