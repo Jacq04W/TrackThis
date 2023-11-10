a0334bfb96ac7f2936a08b900c9407126a472287
Jacquese Whitson <jacqwhitsonappledev@gmail.com>
2023-11-10 10:24:48 -0500

    added onBoarding view

---
+++ TrackThis/OnBoarding.swift
@@ -0,0 +1,18 @@
+//
+//  OnBoarding.swift
+//  TrackThis
+//
+//  Created by Jacquese Whitson on 11/10/23.
+//
+
+import SwiftUI
+
+struct OnBoarding: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnBoarding()
+}
---
+++ TrackThis/onb1.swift
@@ -0,0 +1,18 @@
+//
+//  onb1.swift
+//  TrackThis
+//
+//  Created by Jacquese Whitson on 11/10/23.
+//
+
+import SwiftUI
+
+struct onb1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    onb1()
+}
